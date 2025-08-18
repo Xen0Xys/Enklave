ddc0b30f7f8915510f902c061070cb45441d9716
Xen0Xys <red57101@gmail.com>
2025-08-18 11:25:13 +0200

    :lipstick: Add definitive theming

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: {enabled: true},
-    modules: ["@nuxt/icon", "shadcn-nuxt"],
+    modules: ["@nuxt/icon", "shadcn-nuxt", "@nuxtjs/google-fonts"],
     css: ["./app/assets/css/tailwind.css"],
     vite: {
         plugins: [tailwindcss() as unknown as any],
@@ -13,5 +13,12 @@ export default defineNuxtConfig({
     shadcn: {
         prefix: "",
         componentDir: "./app/components/ui",
+    },
+    googleFonts: {
+        families: {
+            Sora: true,
+            Inter: true,
+            "JetBrains Mono": true,
+        },
     },
 });
