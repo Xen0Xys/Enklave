87e9026ac5561a0ec992b52e340d3d5241afa5bc
Xen0Xys <red57101@gmail.com>
2025-09-14 16:14:13 +0200

    :bug: Minor fixes

--- website/app/app.vue
+++ website/app/app.vue
@@ -31,7 +31,7 @@ useSeoMeta({
 
 useHead({
     link: [
-        {rel: "icon", type: "image/x-icon", href: "/icon.webp"},
+        {rel: "icon", type: "image/webp", href: "/icon.webp"},
         {rel: "canonical", href: "https://enklave.cloud"},
         {rel: "preconnect", href: "https://fonts.googleapis.com"},
         {rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: ""},
@@ -46,7 +46,7 @@ useHead({
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1" />
-            <link rel="icon" type="image/x-icon" href="/icon.webp" />
+            <link rel="icon" type="image/webp" href="/icon.webp" />
         </Head>
         <Body>
             <NuxtLoadingIndicator />
--- website/content/newsletter/website-first-version.md
+++ website/content/newsletter/website-first-version.md
@@ -8,10 +8,10 @@ readTime: 2min
 category: "Newsletter"
 ---
 
-# 🎉 First version of the Enklave website is now live !
+# 🎉 First version of the Enklave website is now live!
 
 Welcome to Enklave ! The first version of our website is now online. You can now explore our features, discover security tips, and enjoy a modern experience to protect your passwords and sensitive documents.
 
 Feel free to share your feedback and check the "Newsletter" section regularly to stay updated on new features and security tips.
 
-Thank you for your trust and happy exploring !
+Thank you for your trust and happy exploring!
